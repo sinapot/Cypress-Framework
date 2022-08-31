c7bde61a59104dce3a4ffd3febf513562930b674
sinapot <omai_edison@yahoo.com>
2022-08-31 18:50:42 +1200

    renamed session to percyVisual test

---
+++ cypress/e2e/percyVisualTests.js
@@ -0,0 +1,32 @@
+/// <reference types="cypress" />
+
+
+describe('using custom command and session', () => {
+
+
+    before ( () => {
+        //this is a custom command
+        cy.login('alon@sharklasers.com','alonalon')
+    })
+
+    it('going to cart without loggin in', () => {
+        //the session saved in the custom command will be saved and used by the following command
+        //user no longer need to login again to go to below URL
+        cy.visit('/?controller=order')
+        cy.percySnapshot("Cart")
+    })
+
+    it('going to Contacts without loggin in', () => {
+        //the session saved in the custom command will be saved and used by the following command
+        //user no longer need to login again to go to below URL
+        cy.visit('/?controller=contact')
+        cy.percySnapshot("Contacts")
+    })
+
+    it('going to Womans without loggin in', () => {
+        //the session saved in the custom command will be saved and used by the following command
+        //user no longer need to login again to go to below URL
+        cy.visit('/id_category=3&controller=category')
+        cy.percySnapshot("Womans Category")
+    })
+})
